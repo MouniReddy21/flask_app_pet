a19571da9986eedf474d80a1299e7bc5e5f0f748
MouniReddy21 <81528901+MouniReddy21@users.noreply.github.com>
2024-09-21 08:34:06 -0400

    Initial commit for Flask Pet App

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from forms import PetForm
+
+app = Flask(__name__)  #create instance of flask class, __name__tells where to look for resources.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pets.db' #specifying the database file pets.db
+app.config['SECRET_KEY'] = 'your_secret_key' #configuration of secret key for session management
+db = SQLAlchemy(app) #creating SQLAlchemy instance that allows interation with database
+
+# Import models after db is created to avoid circular imports
+from models import Pet  #this defines database schema for pets
+
+@app.route('/')  # Route for the home page, renders home.html
+def home():
+    return render_template('home.html')
+
+@app.route('/pets', methods=['GET', 'POST']) # route for adding pets
+def add_pet():
+    form = PetForm()
+    if form.validate_on_submit(): # checks if the form is submitted and valid
+        new_pet = Pet(name=form.name.data, species=form.species.data)
+        db.session.add(new_pet) # add new pet to database session
+        db.session.commit() #commit the database and saves new pet to the database
+        return redirect(url_for('pet_list')) # redirects to pet_list route
+    return render_template('add_pet.html', form=form) # If the form is not submitted/invalid, it renders the add_pet.html template, passing the object for rendering.
+
+@app.route('/pet_list')
+def pet_list():
+    pets = Pet.query.all()  # Queries the database for all the pets
+    return render_template('pet_list.html', pets=pets) # renders the pet_list by passing the pets
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
